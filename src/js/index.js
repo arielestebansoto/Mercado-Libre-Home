17a7c0e6b6f5633388e3756694f1d8229a281817
Ariel Soto <arielestebansoto@gmail.com>
2021-03-06 14:36:51 -0300

    refactoring js for svgs category__box

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
-import { svg1 }  from './svgCategories.js'
+//to insert the category__box elements
 
-var categoryBox = document.querySelector('.category__box')
-console.log(categoryBox)
-categoryBox.innerHTML = svg1.svg
+import { svgs } from './svgCategories.js'
+
+for (var i = 0; i < 14; i++) {
+    let categoryBox = document.querySelector(`#category__box${i+1}`)
+    let categoryBoxSvgContainer = categoryBox.querySelector('.category__box--svg-container')
+
+    let categoryBoxTitle = categoryBox.querySelector('.category__box--title')
+
+    categoryBoxSvgContainer.innerHTML = svgs().svg1.svg
+    categoryBoxTitle.innerHTML = svgs().svg1.name
+    console.dir(svgs)
+}
